2eafc5b4d97b7b780c971a16ef650ea4a3ee21fb
Zulfadlifauzi <fauzi_zulfadli@yahoo.com>
2021-08-28 22:03:58 +0800

    First Commit locategeouser

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Position? _currentPosition;
+  String? _currentAddress;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Location'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            if (_currentAddress != null) Text(_currentAddress!),
+            TextButton(
+              child: Text('Get Location'),
+              onPressed: () {
+                _getCurrentLocation();
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _getCurrentLocation() {
+    Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.best,
+            forceAndroidLocationManager: true)
+        .then((Position position) {
+      setState(() {
+        _currentPosition = position;
+        _getAddressFromLatLng();
+      });
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
+  _getAddressFromLatLng() async {
+    try {
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+          _currentPosition!.latitude, _currentPosition!.longitude);
+      Placemark place = placemarks[0];
+      setState(() {
+        _currentAddress =
+            '${place.locality}, ${place.postalCode}, ${place.country}, ${place.street}';
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+}
