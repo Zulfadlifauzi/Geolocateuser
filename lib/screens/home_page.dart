29dfe618907615a08448d85f1821e5883975d175
Zulfadlifauzi <fauzi_zulfadli@yahoo.com>
2021-09-17 19:03:00 +0800

    add, lottie,button for post API.

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:geocoding/geocoding.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,27 +10,122 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-
 class _HomeScreenState extends State<HomeScreen> {
   Position? _currentPosition;
-  String? _currentAddress;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('Location'),
+        title: Text(
+          'Locating user location',
+          style: GoogleFonts.varela(color: Colors.black),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.white,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Stack(
           children: <Widget>[
-            if (_currentAddress != null) Text(_currentAddress!),
-            TextButton(
-              child: Text('Get Location'),
-              onPressed: () {
-                _getCurrentLocation();
-              },
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 15),
+                  child: Container(
+                    child: Text(
+                        'Enable your location ! \nFor new live shipping experience',
+                        style: GoogleFonts.varela(
+                          fontSize: 15,
+                        )),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Column(
+              children: [
+                Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.only(top: 80),
+                  child: Lottie.asset(
+                    'assets/lottie/delivery.json',
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 20),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 40, right: 40),
+                  child: Container(
+                    margin: EdgeInsets.only(top: 300),
+                    child: Form(
+                      child: TextFormField(
+                        decoration:
+                            InputDecoration(labelText: 'Enter your name'),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 40, right: 40),
+                child: Container(
+                  margin: EdgeInsets.only(top: 370),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'Location',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      SizedBox(height: 15),
+                      if (_currentPosition != null)
+                        Text(
+                            'Longitude: ${_currentPosition!.longitude} \nLatitude: ${_currentPosition!.latitude}'),
+                      TextButton(
+                        child: Text('Get Location'),
+                        onPressed: () {
+                          _getCurrentLocation();
+                        },
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 40, right: 40),
+                child: Column(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      margin: EdgeInsets.only(top: 500),
+                      decoration: BoxDecoration(
+                          color: Colors.redAccent,
+                          borderRadius: BorderRadius.circular(5.0)),
+                      child: TextButton(
+                        onPressed: () {
+                          Text('Create shippng');
+                          print(Text);
+                        },
+                        child: Text(
+                          'Start Shipping',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
             )
           ],
         ),
@@ -44,24 +140,9 @@ class _HomeScreenState extends State<HomeScreen> {
         .then((Position position) {
       setState(() {
         _currentPosition = position;
-        _getAddressFromLatLng();
       });
     }).catchError((e) {
       print(e);
     });
-  }
-
-  _getAddressFromLatLng() async {
-    try {
-      List<Placemark> placemarks = await placemarkFromCoordinates(
-          _currentPosition!.latitude, _currentPosition!.longitude);
-      Placemark place = placemarks[0];
-      setState(() {
-        _currentAddress =
-            '${place.locality}, ${place.postalCode}, ${place.country}, ${place.street}';
-      });
-    } catch (e) {
-      print(e);
-    }
   }
 }
