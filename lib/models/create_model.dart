94d2fa7686e9e7efa75990388924b0b03853e0b6
Zulfadlifauzi <fauzi_zulfadli@yahoo.com>
2021-09-18 18:23:17 +0800

    create models API POST

--- lib/models/create_model.dart
+++ lib/models/create_model.dart
@@ -1,15 +1,48 @@
 class CreateUserResponse {
-  String? name,latitude,longitude,id,createdAt;
+  String? message;
+  Data? data;
 
-  CreateUserResponse({this.name,this.latitude,this.longitude,this.id,this.createdAt});
+  CreateUserResponse({this.message, this.data});
 
-  factory CreateUserResponse.fromjson(Map<String, dynamic> json) {
-    return CreateUserResponse(
-      name: json['name'],
-      latitude: json['latitude'],
-      longitude: json['longitude'],
-      id: json['id'],
-      createdAt: json['createdAt']
-    );
-  }
+  factory CreateUserResponse.fromJson(Map<String, dynamic> json) =>
+      CreateUserResponse(
+          message: json['message'], data: Data.fromJson(json['data']));
+
+  Map<String, dynamic> toJson() => {'message': message, 'data': data!.toJson()};
+}
+
+class Data {
+  Data({
+    this.latitude,
+    this.longitude,
+    this.name,
+    this.updatedAt,
+    this.createdAt,
+    this.id,
+  });
+
+  String? latitude;
+  String? longitude;
+  String? name;
+  DateTime? updatedAt;
+  DateTime? createdAt;
+  int? id;
+
+  factory Data.fromJson(Map<String, dynamic> json) => Data(
+        latitude: json["latitude"],
+        longitude: json["longitude"],
+        name: json["name"],
+        updatedAt: DateTime.parse(json["updated_at"]),
+        createdAt: DateTime.parse(json["created_at"]),
+        id: json["id"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "latitude": latitude,
+        "longitude": longitude,
+        "name": name,
+        "updated_at": updatedAt!.toIso8601String(),
+        "created_at": createdAt!.toIso8601String(),
+        "id": id,
+      };
 }
