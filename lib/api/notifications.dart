4f2080ec38c2ecfed13de30538b37da5331c935e
Zulfadlifauzi <fauzi_zulfadli@yahoo.com>
2021-10-14 13:47:59 +0800

    add local push notif

---
+++ lib/api/notifications.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:get/get.dart';
+// import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest.dart' as tz;
+
+class NotifyHelper {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin(); //
+
+  initializeNotification() async {
+    tz.initializeTimeZones();
+    final IOSInitializationSettings initializationSettingsIOS =
+        IOSInitializationSettings(
+            requestSoundPermission: false,
+            requestBadgePermission: false,
+            requestAlertPermission: false,
+            onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('icon');
+
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+      iOS: initializationSettingsIOS,
+      android: initializationSettingsAndroid,
+    );
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: selectNotification);
+  }
+
+  Future selectNotification(String? payload) async {
+    if (payload != null) {
+      print('notification payload: $payload');
+    } else {
+      print("Notification Done");
+    }
+    Get.to(() => Container(
+          color: Colors.white,
+        ));
+  }
+
+  displayNotification({required String title, required String body}) async {
+    print("Successfully");
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        title, body,
+        importance: Importance.max, priority: Priority.high);
+    var iOSPlatformChannelSpecifics = const IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        android: androidPlatformChannelSpecifics,
+        iOS: iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      title,
+      body,
+      platformChannelSpecifics,
+      payload: 'Default_Sound',
+    );
+  }
+
+  Future scheduledNotification(
+      {required String title, required String body}) async {
+    var interval = RepeatInterval.everyMinute;
+    var bigPicture = BigPictureStyleInformation(
+        DrawableResourceAndroidBitmap("icon"),
+        largeIcon: DrawableResourceAndroidBitmap("icon"),
+        contentTitle: '$title',
+        summaryText: '$body',
+        htmlFormatContent: true,
+        htmlFormatContentTitle: true);
+
+    var android = AndroidNotificationDetails("id", "channel",
+        styleInformation: bigPicture);
+
+    var platform = new NotificationDetails(android: android);
+
+    await flutterLocalNotificationsPlugin.periodicallyShow(
+        0, title, body, interval, platform);
+  }
+
+  Future cancelAllNotifications() async {
+    await flutterLocalNotificationsPlugin.cancelAll();
+  }
+
+  void requestIOSPermissions() {
+    flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            IOSFlutterLocalNotificationsPlugin>()
+        ?.requestPermissions(
+          alert: true,
+          badge: true,
+          sound: true,
+        );
+  }
+
+  Future onDidReceiveLocalNotification(
+      int? id, String? title, String? body, String? payload) async {
+    Get.dialog(const Text('Welcome to flutter'));
+    // display a dialog with the notification details, tap ok to go to another page
+    // showDialog(
+    //   //context: context,
+    //   builder: (BuildContext context) => CupertinoAlertDialog(
+    //     title: Text(title),
+    //     content: Text(body),
+    //     actions: [
+    //       CupertinoDialogAction(
+    //         isDefaultAction: true,
+    //         child: Text('Ok'),
+    //         onPressed: () async {
+    //           Navigator.of(context, rootNavigator: true).pop();
+    //           await Navigator.push(
+    //             context,
+    //             MaterialPageRoute(
+    //               builder: (context) => SecondScreen(payload),
+    //             ),
+    //           );
+    //         },
+    //       )
+    //     ],
+    //   ),
+    // );
+  }
+}
---
+++ lib/screens/localpn.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:locategeouser/api/notifications.dart';
+import 'package:locategeouser/ui/theme.dart';
+import 'package:lottie/lottie.dart';
+
+class LocalPush extends StatefulWidget {
+  const LocalPush({Key? key}) : super(key: key);
+
+  @override
+  _LocalPushState createState() => _LocalPushState();
+}
+
+class _LocalPushState extends State<LocalPush> {
+  var notifyHelper;
+  @override
+  void initState() {
+    super.initState();
+    notifyHelper = NotifyHelper();
+    notifyHelper.initializeNotification();
+    notifyHelper.requestIOSPermissions();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: yellowClr,
+      appBar: AppBar(
+        title: const Text('Push Notifications'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(left: 20, top: 200),
+              height: 100,
+              width: double.infinity,
+              child: Lottie.asset('assets/lottie/noti.json'),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(left: 20),
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: TextButton(
+                    onPressed: () {
+                      notifyHelper.displayNotification(
+                          body: 'Notification Activated', title: 'Fauzi');
+                    },
+                    child: const Text(
+                      'Simple Notification',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(left: 20),
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: TextButton(
+                    onPressed: () {
+                      notifyHelper.scheduledNotification(
+                          body: 'Scheduled Notification Activated',
+                          title: 'Fauzi');
+                    },
+                    child: const Text(
+                      'Schedule Notification',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(left: 20),
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: TextButton(
+                    onPressed: () {
+                      notifyHelper.cancelAllNotifications();
+                    },
+                    child: const Text(
+                      'Remove Notification',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
